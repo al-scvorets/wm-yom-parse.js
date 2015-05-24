3b61d7a44a5b70655b472a6e3eb2d9c3418e7387
Alexander <al-scvorets>
2015-05-24 18:00:30 +0300

    Multiline node texts, comments, hashes

--- wm-yom-parse.js
+++ wm-yom-parse.js
@@ -1,27 +1,31 @@
 /*!
- * wm-yom-parse.js  v0.1.0
+ * wm-yom-parse.js  v0.2.0
  * Parses YOM source and returns JS object
  *
  * Copyright (c) 2015 http://wamer.net
  * Released under the MIT license
  *
- * Latest Update: 2015-05-01
+ * Latest Update: 2015-05-24
  */
 
 (function (window, undefined) {
-var _version_     = '0.1.0',
+var _version_     = '0.2.0',
     last_error    = null,
     _entry_point_ = function (arg_str) {
 
-    var src_str = arg_str, _ptr = 0, _row = 1, _col = 1;
+    var src_str = arg_str.replace (/(\r\n|\r)/gm, '\n')    // replace CR
+                         .replace (/[\\\+\-]\n/gm, ' '),   // do lines continuation
+
+        _ptr = 0, _row = 1, _col = 1;
     
     last_error = null;
 
     if (!arg_str || typeof arg_str != 'string') return [];
 
     var sym = {_EOF: -1, _SEP: -2,
-               _GROUP_START: -3, _GROUP_ENF: -4,
-               _ATTR_START: -5, _ATTR_END: -6};
+               _GROUP_START: -3, _GROUP_END: -4,
+               _ATTR_START: -5, _ATTR_END: -6,
+               _HASH: -7};
 
     function chr (lit) {
         var ch = src_str.charAt (_ptr);
@@ -33,28 +37,18 @@ var _version_     = '0.1.0',
         if (lit) return _advance (ch);  // treat literally
 
         // tok end
-        if ({'\t':1, '\b':1, '\f':1, ',': 1}[ch]) return _advance (sym._SEP);
+        if ({'\n':1, '\t':1, '\b':1, '\f':1, ',': 1}[ch])
+            return _advance (sym._SEP);
 
         // escape
         if ('/' == ch) {_advance();  return chr (true);}
 
         // spec syms
-        var t = {'[': sym._GROUP_START, ']': sym._GROUP_ENF,
-                 '{': sym._ATTR_START,  '}': sym._ATTR_END}[ch];
+        var t = {'[': sym._GROUP_START, ']': sym._GROUP_END,
+                 '{': sym._ATTR_START,  '}': sym._ATTR_END,
+                 '#': sym._HASH}[ch];
 
         if (t) return _advance (t);
-
-        // LF
-        if        ('\r' == ch) {
-            _ptr++; if (src_str.charAt (_ptr) == '\n') _ptr++;
-            _row++; _col = 1;
-            return sym._SEP;
-            
-        } else if ('\n' == ch) {
-            _ptr++; if (src_str.charAt (_ptr) == '\r') _ptr++;
-            _row++; _col = 1;
-            return sym._SEP;
-        }
 
         // default
         return _advance (ch);
@@ -64,7 +58,7 @@ var _version_     = '0.1.0',
         last_error = {msg: msg, row: _row, col: _col - 1};
     }
 
-    function parse_data () {
+    function parse_attributes () {
         function parse_token (tok) {
             var t = tok.split(/[:=]/, 2);
             return {
@@ -85,11 +79,9 @@ var _version_     = '0.1.0',
             case sym._SEP:
             case sym._ATTR_END:
                 if (_curr_token) {
-                    if (_curr_token) {
-                        t = parse_token (_curr_token);
-                        if (t.name) _result[t.name] = t.val;
-                        _curr_token = '';
-                    }
+                    t = parse_token (_curr_token);
+                    if (t.name) _result[t.name] = t.val;
+                    _curr_token = '';
                 }
                 
                 if (sym._ATTR_END == ch) ch = null;
@@ -99,7 +91,7 @@ var _version_     = '0.1.0',
                 set_err ('Unexpected \'[\''); ch = null;  _result = null;
                 break;
 
-            case sym._GROUP_ENF:
+            case sym._GROUP_END:
                 set_err ('Unexpected \']\''); ch = null;  _result = null;
                 break;
 
@@ -115,8 +107,29 @@ var _version_     = '0.1.0',
         return _result;
     }
 
+    function parse_hash () {
+        var ch = chr();
+        if (' ' === ch) return ' ';  // comment start
+
+        if (!ch.match (/[a-zA-Z_$]/)) {
+            set_err ('Bad char in hash: ' + ch);
+            return undefined;
+        }
+
+        var _result = ch;
+
+        do {
+            ch = chr (true);
+            if (ch.match(/[0-9a-zA-Z_$]/)) _result += ch;
+            else                           ch = null;   
+        } while (ch);
+
+        var _r = {};  _r[_result] = true; // object... for mustache conv (?)
+        return _r;
+    }
+
     function new_node (p) {
-        var n = {parent: p, label: '', nodes:[]};
+        var n = {parent: p, text: '', nodes:[]};
         if (p) p.nodes.push (n);
         return n;
     }
@@ -126,10 +139,11 @@ var _version_     = '0.1.0',
         ch;
 
     function flush_node () {
-        _curr_node.label = $.trim (_curr_node.label);
-        if (!_curr_node.label &&
-            !_curr_node.attr  &&
-            !_curr_node.nodes.length) {
+        _curr_node.text = $.trim (_curr_node.text);
+        if (!_curr_node.text &&             // no content
+            !_curr_node.attr &&
+            !_curr_node.nodes.length ||     // or comment
+            ' ' === _curr_node.hash) {
 
             _curr_node.parent.nodes.pop();
         }
@@ -142,16 +156,20 @@ var _version_     = '0.1.0',
             ch = 0;
             break;
 
-        case sym._SEP:   
+        case sym._SEP:
+        case sym._HASH:
             flush_node();
             _curr_node = new_node (_curr_node.parent);
+
+            if (sym._HASH == ch) 
+                _curr_node.hash = parse_hash ();
             break;
 
         case sym._GROUP_START:
             _curr_node = new_node (_curr_node);
             break;
 
-        case sym._GROUP_ENF:
+        case sym._GROUP_END:
             flush_node();
             _curr_node = _curr_node.parent;
             if (!_curr_node) {
@@ -160,7 +178,7 @@ var _version_     = '0.1.0',
             break;
                       
         case sym._ATTR_START:
-            _curr_node.attr = parse_data ();
+            _curr_node.attr = parse_attributes ();
             if (!_curr_node.attr) ch = null;  // some errors
             break;
 
@@ -169,7 +187,7 @@ var _version_     = '0.1.0',
             break;
 
         default:
-            _curr_node.label += ch;
+            _curr_node.text += ch;
         }
     } while (ch);
 
